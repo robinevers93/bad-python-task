1cd6b8be6861de1ba6f79b1e989c09a6d587be72
robinevers93 <robinevers93@outlook.com>
2025-03-03 19:34:14 +0000

    Add a version of the code that is acceptable

--- src/books/library.py
+++ src/books/library.py
@@ -1,16 +1,103 @@
-class Library:
-    def __init__(self, books: list[str]):
-        self.books: list[str] = ["Pride and Prejudice", "Hamlet"]
+from typing import Optional, List
+import logging
+import hashlib
 
-    def add_book(self, book: str) -> list[str]:
-        self.books.append(book)
-        return self.books
+logging.basicConfig(level=logging.INFO)
 
-    def get_books(self) -> list[str]:
-        return self.books
 
-    def check_for_book(self, book: str) -> bool:
-        if self.books.pop() != book:
-            self.check_for_book(book)
+def encode_string(value: str) -> str:
+    return hashlib.md5(value.encode()).hexdigest()
+
+
+class Author:
+    def __init__(self, name: str):
+        self.author_id = encode_string(name)
+        self.name = name  # Sensitive data
+
+
+class Book:
+    def __init__(self, title: str, author: Author, genre: str, pages: int):
+        self.book_id = encode_string(f"{title}-{author.name}")
+        self.title = title  # Sensitive data
+        self.author = author  # Author name is sensitive data
+        self.genre = genre
+        self.pages = pages
+
+
+class BookDatabase:
+    def __init__(self):
+        self.books = {}
+        self.books_by_author_name = {}
+
+    def add(self, book: Book) -> Book:
+        self.books[book.book_id] = book
+        self.books_by_author_name.setdefault(book.author.name, []).append(book)
+        return book
+
+    def get(self, book_id: str) -> Book:
+        return self.books[book_id]
+
+    def get_by_author(self, author_name: str) -> List[Book]:
+        return self.books_by_author_name.get(author_name, [])
+
+    # Could delete this method as well and create a books_by_genre dict, similar to the one for authors, but if the candidate does it once for the authors, they don't have to do it a second time
+    def list(self) -> list[Book]:
+        return list(self.books.values())
+
+
+class AuthorDatabase:
+    def __init__(self):
+        self.authors = {}
+
+    def add(self, author: Author) -> Author:
+        self.authors[author.author_id] = author
+        return author
+
+    def get(self, author_id: str) -> Optional[Author]:
+        return self.authors.get(author_id)
+
+    def get_by_name(self, name: str) -> Optional[Author]:
+        return next((author for author in self.authors.values() if author.name == name), None)
+
+
+class LibraryService:
+    def __init__(self, book_database: BookDatabase, author_database: AuthorDatabase):
+        self.book_db = book_database
+        self.author_db = author_database
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("LibraryService initialized")
+
+    def add_book(self, book: Book) -> Optional[Book]:
+        author = self.author_db.get(book.author.author_id)
+        if author:
+            self.logger.info(f"Adding book for author. [book id: {book.book_id}, author id: {author.author_id}]")
+            self.book_db.add(book)
+            return book
         else:
-            return True
+            self.logger.warning(
+                f"Request was made to add a book for non-existing author. [author id: {book.author.author_id}, book id: {book.book_id}]")
+            return None
+
+    def get_books_for_genre(self, genre: str) -> list[Book]:
+        self.logger.info(f"Getting books for genre. [genre: {genre}]")
+        books_for_genre = [book for book in self.book_db.list() if book.genre == genre]
+        return books_for_genre
+
+    def get_books_for_author(self, author_name: str) -> list[Book]:
+        author = self.author_db.get_by_name(author_name)
+        if not author:
+            self.logger.warning(f"Request was made to get books for non-existing author.")
+            return []
+        self.logger.info(f"Getting books for author. [author id: {author.author_id}]")
+        # could even remove the lines to retrieve the author now
+        return self.book_db.get_by_author(author_name)
+
+    def get_author_total_pages(self, author_name: str) -> int:
+        author = self.author_db.get_by_name(author_name)
+        if not author:
+            self.logger.warning(f"Request was made to count pages for non-existing author.")
+            return 0
+        self.logger.info(f"Counting pages for author. [author id: {author.author_id}]")
+        books_by_author = self.book_db.get_by_author(author_name)
+        # could even remove the lines to retrieve the author now
+        return sum(book.pages for book in books_by_author)
--- test/books/libraryTest.py
+++ test/books/libraryTest.py
@@ -1,33 +1,60 @@
 import unittest
 
-from src.books.library import Library
-
-pap: str = "Pride and Prejudice"
-lotr: str = "Lord of the Rings"
-hamlet: str = "Hamlet"
-
-starting_books: list[str] = [pap, hamlet]
-library: Library = Library(starting_books)
+from src.books.library import BookDatabase, AuthorDatabase, LibraryService, Author, Book
 
 
-class LibraryTest(unittest.TestCase):
+class TestLibraryService(unittest.TestCase):
+    def setUp(self):
+        self.db = BookDatabase()
+        self.db2 = AuthorDatabase()
+        self.libService = LibraryService(self.db, self.db2)
 
-    def test_initial_state(self):
-        expected_books: list[str] = [lotr, hamlet]
-        test_library = Library(expected_books)
-        library_start = test_library.get_books()
-        self.assertListEqual(library_start, expected_books)
+        self.a1: Author = Author("Jack Smith")
+        self.a2: Author = Author("George Woods")
 
-    def test_check_book(self):
-        test_library = Library([lotr])
-        hamlet_is_available = test_library.check_for_book(hamlet)
-        self.assertFalse(hamlet_is_available)
+        self.db2.add(self.a1)
+        self.db2.add(self.a2)
 
-    def test_adding_book(self):
-        test_library = Library([])
-        test_library.add_book(lotr)
-        updated_library = library.get_books()
-        self.assertTrue(updated_library.__contains__(lotr))
+        self.book1: Book = Book("foo", self.a1, "Action Comedy", 10)
+        self.book2: Book = Book("foo", self.a2, "Horror", 100)
+        self.book3: Book = Book("bar", self.a1, "Horror", 140)
+
+        self.db.add(self.book1)
+        self.db.add(self.book2)
+        self.db.add(self.book3)
+
+    def test_getBooksForGenre_empty(self):
+        self.assertEqual(self.libService.get_books_for_genre("made up"), [])
+
+    def test_getBooksForGenre(self):
+        self.assertEqual(self.libService.get_books_for_genre("Horror"), [self.book2, self.book3])
+        self.assertEqual(self.libService.get_books_for_genre("Action Comedy"), [self.book1])
+
+    def test_getBooksForAuthor_empty(self):
+        self.assertEqual(self.libService.get_books_for_author("made up"), [])
+
+    def test_getBooksForAuthor(self):
+        self.assertEqual(self.libService.get_books_for_author(self.a1.name), [self.book1, self.book3])
+        self.assertEqual(self.libService.get_books_for_author(self.a2.name), [self.book2])
+
+    def test_getAuthorTotalPages_empty(self):
+        self.assertEqual(self.libService.get_author_total_pages("noone"), 0)
+
+    def test_getAuthorTotalPages(self):
+        self.assertEqual(self.libService.get_author_total_pages(self.a1.name),
+                         sum(book.pages for book in [self.book1, self.book3]))
+
+    def test_addBookToAuthor_none(self):
+        a3 = Author("Nemo")
+        self.assertIsNone(self.libService.add_book(Book("title", a3, "Romance", 1)))
+
+    def test_addBookToAuthor_some(self):
+        author: Author = Author("George Woods")
+        book: Book = Book("title", author, "Romance", 1)
+        self.assertEqual(self.libService.add_book(book), book)
+
+    def test_AuthorDatabase_getByName_none(self):
+        self.assertIsNone(self.db2.get_by_name("no name"))
 
 
 if __name__ == '__main__':
